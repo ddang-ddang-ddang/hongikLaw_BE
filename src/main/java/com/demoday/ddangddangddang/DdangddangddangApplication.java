202a9c54f8f559f75345721cc79fa5185c2ef728
최우혁 <whc9999@naver.com>
2025-09-27 00:30:03 +0900

    [Feat] 초기 프로젝트 파일 생성

---
+++ src/main/java/com/demoday/ddangddangddang/DdangddangddangApplication.java
@@ -0,0 +1,13 @@
+package com.demoday.ddangddangddang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdangddangddangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdangddangddangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoday/ddangddangddang/DdangddangddangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoday.ddangddangddang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdangddangddangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
