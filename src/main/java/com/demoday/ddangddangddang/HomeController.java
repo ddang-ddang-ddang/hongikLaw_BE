c74f05e7be17e07932736f4cfea655cbe166e36f
shinae <shinae1023@naver.com>
2025-10-09 17:18:21 +0900

    [Fix] import 추가

--- src/main/java/com/demoday/ddangddangddang/HomeController.java
+++ src/main/java/com/demoday/ddangddangddang/HomeController.java
@@ -3,6 +3,7 @@ package com.demoday.ddangddangddang;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RequiredArgsConstructor
 @RestController
