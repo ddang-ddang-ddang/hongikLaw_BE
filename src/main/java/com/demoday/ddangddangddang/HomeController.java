bf308109d3b10c00d3760047a196bdfef2191dad
shinae <shinae1023@naver.com>
2025-10-09 17:15:17 +0900

    [Feat] 스웨거 및 에러 핸들러 설정 완료

---
+++ src/main/java/com/demoday/ddangddangddang/HomeController.java
@@ -0,0 +1,16 @@
+package com.demoday.ddangddangddang;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    @Operation(summary = "스웨거테스트")
+    public String home() {
+        return "멋쟁이사자처럼 신촌연합 데모데이 땅땅땅 서버입니다.";
+    }
+}
---
+++ src/main/java/com/demoday/ddangddangddang/global/code/GeneralErrorCode.java
@@ -0,0 +1,32 @@
+package com.demoday.ddangddangddang.global.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum GeneralErrorCode implements BaseErrorCode {
+
+    // 인증 에러
+    DUPLICATE_LOGINID(HttpStatus.BAD_REQUEST,"AUTH_4001","중복되는 아이디가 존재합니다."),
+    MISSING_AUTH_INFO(HttpStatus.UNAUTHORIZED, "AUTH_4011", "인증 정보가 누락되었습니다."),
+    INVALID_LOGIN(HttpStatus.UNAUTHORIZED, "AUTH_4012", "올바르지 않은 아이디, 혹은 비밀번호입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_4012", "유효하지 않은 토큰입니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "AUTH_4031", "접근 권한이 없습니다."),
+    TOKEN_EXPIRED(HttpStatus.valueOf(419), "AUTH_4191", "토큰이 만료되었습니다."),
+
+    // 서버 내부 에러
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SERVER_5001", "서버 내부 오류입니다."),
+    SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "SERVER_5031", "서버가 일시적으로 불안정합니다."),
+    EXTERNAL_SERVICE_TIMEOUT(HttpStatus.GATEWAY_TIMEOUT, "SERVER_5041", "외부 서비스 응답 지연"),
+
+    //요청 파라미터 에러
+    MISSING_PARAMETER(HttpStatus.BAD_REQUEST, "REQ_4001", "필수 파라미터가 누락되었습니다."),
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "REQ_4002", "파라미터 형식이 잘못되었습니다."),
+    UNSUPPORTED_CONTENT_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "REQ_4151", "지원하지 않는 Content-Type입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
