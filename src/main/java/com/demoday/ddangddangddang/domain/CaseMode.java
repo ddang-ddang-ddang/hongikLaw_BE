f54a54db4b6735fdee278d0c8f8873e32c205680
최우혁 <whc9999@naver.com>
2025-10-28 15:49:19 +0900

    [Feat] enum 추가
    -CaseMode, DebateSide, JudgmentStage

---
+++ src/main/java/com/demoday/ddangddangddang/domain/CaseMode.java
@@ -0,0 +1,5 @@
+package com.demoday.ddangddangddang.domain;
+
+public enum CaseMode {
+    SOLO, PARTY
+}
---
+++ src/main/java/com/demoday/ddangddangddang/domain/DebateSide.java
@@ -0,0 +1,5 @@
+package com.demoday.ddangddangddang.domain;
+
+public enum DebateSide {
+    A, B
+}
