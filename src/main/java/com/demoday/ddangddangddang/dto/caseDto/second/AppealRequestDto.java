c46ab956a581925c5620afd1e188111f352c83ee
황신애 <shinae1023@naver.com>
2025-11-17 16:58:55 +0900

    Add Swagger annotation to AppealRequestDto

--- src/main/java/com/demoday/ddangddangddang/dto/caseDto/second/AppealRequestDto.java
+++ src/main/java/com/demoday/ddangddangddang/dto/caseDto/second/AppealRequestDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Getter
 @NoArgsConstructor
