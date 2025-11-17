6f8734d0b915c4ab8e1b917680aaa2609442a779
황신애 <shinae1023@naver.com>
2025-11-17 16:31:36 +0900

    Add JsonFormat annotation to AppealRequestDto

--- src/main/java/com/demoday/ddangddangddang/dto/caseDto/second/AppealRequestDto.java
+++ src/main/java/com/demoday/ddangddangddang/dto/caseDto/second/AppealRequestDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Getter
 @NoArgsConstructor
