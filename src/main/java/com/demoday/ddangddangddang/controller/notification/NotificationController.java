96919fc7f6ec63903d8850afe03528f171054969
shinae <shinae1023@naver.com>
2025-11-19 19:57:23 +0900

    [Fix] 컨트롤러에 토큰 필요 명시

--- src/main/java/com/demoday/ddangddangddang/controller/notification/NotificationController.java
+++ src/main/java/com/demoday/ddangddangddang/controller/notification/NotificationController.java
@@ -3,6 +3,7 @@ package com.demoday.ddangddangddang.controller.notification;
 import com.demoday.ddangddangddang.global.security.UserDetailsImpl;
 import com.demoday.ddangddangddang.global.sse.SseEmitters;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -23,6 +24,7 @@ public class NotificationController {
     // 클라이언트에서 EventSource로 연결할 엔드포인트
     @Operation(summary = "알림 구독", description = "알림을 전송합니다")
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @SecurityRequirement(name = "JWT TOKEN")
     public SseEmitter subscribe(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return sseEmitters.connectUser(userDetails.getUser().getId());
     }
