2d62e13267fb67b5f658c4043e985e87fad586bc
황신애 <shinae1023@naver.com>
2025-11-23 16:27:55 +0900

    [fix] 등업 경험치 조정

--- src/main/java/com/demoday/ddangddangddang/domain/enums/Rank.java
+++ src/main/java/com/demoday/ddangddangddang/domain/enums/Rank.java
@@ -5,21 +5,21 @@ import lombok.Getter;
 
 @Getter
 public enum Rank {
-    PARTNER_LAWYER("파트너 변호사", 6000L),
-    SENIOR_LAWYER("시니어 변호사", 5200L),
-    MID_LEVEL_LAWYER("중견 변호사", 4500L),
-    JUNIOR_LAWYER("신입 변호사", 3850L),
+    PARTNER_LAWYER("파트너 변호사", 8500L),
+    SENIOR_LAWYER("시니어 변호사", 6500L),
+    MID_LEVEL_LAWYER("중견 변호사", 5000L),
+    JUNIOR_LAWYER("신입 변호사", 4100L),
 
-    LAW_SCHOOL_GRADUATE("로스쿨 졸업반", 3250L),
-    LAW_SCHOOL_2L("로스쿨 2학년", 2700L),
-    LAW_SCHOOL_1L("로스쿨 1학년", 2200L),
+    LAW_SCHOOL_GRADUATE("로스쿨 졸업반", 3650L),
+    LAW_SCHOOL_2L("로스쿨 2학년", 3000L),
+    LAW_SCHOOL_1L("로스쿨 1학년", 2400L),
 
-    LAW_STUDENT_SENIOR("법대생 졸업반", 1750L),
-    LAW_STUDENT_JUNIOR("법대생 3학년", 1350L),
+    LAW_STUDENT_SENIOR("법대생 졸업반", 1900L),
+    LAW_STUDENT_JUNIOR("법대생 3학년", 1400L),
     LAW_STUDENT_SOPHOMORE("법대생 2학년", 1000L),
     LAW_STUDENT_FRESHMAN("법대생 1학년", 700L),
 
-    MASTER_BRAWLER("말싸움 고수", 450L),
+    MASTER_BRAWLER("말싸움 고수", 500L),
     MID_BRAWLER("말싸움 중수", 250L),
     NOVICE_BRAWLER("말싸움 하수", 100L),
     NEWBIE_BRAWLER("말싸움 풋내기", 0L);
